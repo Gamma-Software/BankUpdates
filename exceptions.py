e7b39cdbcd4cbbbd5bc478c2b64b6b6438e2e1bc
Valentin Rudloff <gamma_production.support@live.fr>
2020-06-02 23:07:38 +0200

    add bankin interface / raise exceptions
    to be able to autheticate, logout, check the correct login, refresh items, get item ids, get item balance

---
+++ exceptions.py
@@ -0,0 +1,23 @@
+
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+
+class PostGetErrors(Error):
+    errors = {
+        204: "No content",
+        400: "Bad request",
+        401: "Unauthorized",
+        403: "Forbidden",
+        404: "Not found",
+        409: "Conflict",
+        415: "Unsupported media type",
+        422: "Unprocessable entity",
+        429: "Too many Requests",
+        500: "Internal Server Error"
+    }
+
+    def __init__(self, status_code, message):
+        self.expression = status_code
+        self.message = str(status_code) + ": "+self.errors.get(status_code, "Unknown issue") + " " + message
