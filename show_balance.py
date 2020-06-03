c9cf2304ac59f162d1c254025b5f0f6537c5b5bc
Rudloff <valentin.rudloff@navya.tech>
2020-06-03 23:55:14 +0200

    dispatch correctly the différent scripts

---
+++ show_balance.py
@@ -0,0 +1,7 @@
+
+def show_balance():
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    store_balance()
---
+++ store_balance.py
@@ -0,0 +1,20 @@
+from getAccounts.bankin_interface import BankinInterface
+from getAccounts.exceptions import PostGetErrors
+
+
+def store_balance():
+    f = open("login.txt", "r")
+    [email, password, client_id, client_secret, bankin_device, bankin_version] = f.read().splitlines()
+
+    interface = BankinInterface(email, password, client_id, client_secret, bankin_device, bankin_version)
+    try:
+        if interface.authenticate():
+            datas = interface.get_items_balance()
+            interface.logout()
+    except PostGetErrors as error:
+        print("error: " + error.message)
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    store_balance()
