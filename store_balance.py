3ee841f8d69710ceabc8b3019b2844803232d10a
Rudloff <valentin.rudloff@navya.tech>
2020-06-04 00:11:08 +0200

    add comments

--- store_balance.py
+++ store_balance.py
@@ -4,14 +4,18 @@ import log
 
 
 def store_balance():
+    """ Main script to login, refresh the balance, save it in an excel file, logout"""
+
+    # Open the login file and retrieve the personal data to login to Bankin account
     f = open("login.txt", "r")
     [email, password, client_id, client_secret, bankin_device, bankin_version] = f.read().splitlines()
     f.close()
 
+    # Use the Bankin interface to login, refresh the balance, save it in an excel file, logout
     interface = BankinInterface(email, password, client_id, client_secret, bankin_device, bankin_version)
     try:
         if interface.authenticate():
-            datas = interface.get_items_balance()
+            datas = interface.get_items_balance() # Get the latest balance of all the bankin accounts
             interface.logout()
     except PostGetErrors as error:
         print("error: " + error.message)
