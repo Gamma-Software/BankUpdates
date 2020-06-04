c8df1334ae17391f1d3fff17e4144081873982f8
Valentin Rudloff <gamma_production.support@live.fr>
2020-06-05 01:38:19 +0200

    simplify the excel interface

--- excel_interface.py
+++ excel_interface.py
@@ -5,9 +5,7 @@ import pandas as pd
 def save_in_excel(data: pd.DataFrame):
     writer = pd.ExcelWriter('accounts.xlsx', engine='xlsxwriter', datetime_format='mmm d yyyy hh:mm:ss',
                         date_format='mmmm dd yyyy')
-    workbook = writer.book
-    worksheet = writer.sheets['Sheet1']
 
-    data.to_excel("accounts.xlsx", sheet_name='Sheet1')
+    data.to_excel(writer, sheet_name='Sheet1', index=False)
 
     writer.save()
