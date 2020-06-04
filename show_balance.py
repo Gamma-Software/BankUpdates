1c9c2fd9e32821fd92c88092bd4dbd157fa23504
Valentin Rudloff <gamma_production.support@live.fr>
2020-06-04 23:01:21 +0200

    Use plotly

--- show_balance.py
+++ show_balance.py
@@ -1,6 +1,30 @@
+import pandas as pd
 
 def show_balance():
     print("test")
+
+    df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+
+    fig = go.Figure(go.Scatter(
+        x=df['Date'],
+        y=df['mavg']
+    ))
+
+    fig.update_xaxes(
+        rangeslider_visible=True,
+        tickformatstops=[
+            dict(dtickrange=[None, 1000], value="%H:%M:%S.%L ms"),
+            dict(dtickrange=[1000, 60000], value="%H:%M:%S s"),
+            dict(dtickrange=[60000, 3600000], value="%H:%M m"),
+            dict(dtickrange=[3600000, 86400000], value="%H:%M h"),
+            dict(dtickrange=[86400000, 604800000], value="%e. %b d"),
+            dict(dtickrange=[604800000, "M1"], value="%e. %b w"),
+            dict(dtickrange=["M1", "M12"], value="%b '%y M"),
+            dict(dtickrange=["M12", None], value="%Y Y")
+        ]
+    )
+
+    fig.show()
 
 
 if __name__ == "__main__":
