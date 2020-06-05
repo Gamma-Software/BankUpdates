850dd6b5fd702295acf033702065aabc5b8ee899
Valentin Rudloff <gamma_production.support@live.fr>
2020-06-05 15:15:31 +0200

    add banki interface class doc

--- bankin_interface.py
+++ bankin_interface.py
@@ -29,6 +29,7 @@ class BankinInterface:
         self.item = {}
 
     def authenticate(self):
+        """ Authenticate the user"""
         log("Authenticate")
         response = requests.post(self.authenticate_url, headers=self.headers, params=self.params)
         if response.status_code != 200:
@@ -37,7 +38,7 @@ class BankinInterface:
         return self.check_bankin_account()
 
     def refresh_item(self, item_to_refresh):
-        log("Refresh bank account")
+        """ Refresh the items"""
         response = requests.post(self.items_url + item_to_refresh + '/refresh', headers=self.headers)
         if response.status_code != 200:
             raise PostGetErrors(response.status_code, "error raised on refreshing")
@@ -60,17 +61,19 @@ class BankinInterface:
         return True
 
     def get_items_ids(self):
-        self.item = {}
+        """ Get all the items ids"""
         for account in self.get_items_response_json():
             self.item.update({'item': account.get('item').get('id')})
 
     def get_items_response_json(self):
+        """ Get all the items response"""
         response = requests.get(self.accounts_url, headers=self.headers, params=(('limit', '200'),))
         if response.status_code != 200:
             raise PostGetErrors(response.status_code, "error raised on getting the items")
         return response.json()['resources']
 
     def get_items_balance(self):
+        """ Get all the items balance and store them in a useful DataFrame"""
         log("Retrieved items balance")
         account_name = []
         id = []
@@ -89,12 +92,14 @@ class BankinInterface:
         return dataframe_to_return
 
     def logout(self):
+        """ Logout the user"""
         log("logout user")
         response = requests.post(self.logout_url, headers=self.headers)
         if response.status_code != 200:
             raise PostGetErrors(response.status_code, "error raised on logout")
 
     def check_bankin_account(self):
+        """ Check the validity of the user's email"""
         log("check user")
         response = requests.get(self.settings_url, headers=self.headers)
         if response.status_code != 200:
