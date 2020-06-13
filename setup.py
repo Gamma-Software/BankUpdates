24afe1f9f914fe72f8760e457f5148eaad5370db
Valentin Rudloff <valentin.rudloff.perso@gmail.com>
2020-06-13 02:06:34 +0200

    clean the setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="get-account-welpha", # Replace with your own username
+    name="get-account",
     version="0.1.0",
     author="Valentin Rudloff",
     author_email="valentin.rudloff.perso@gmail.com",
     description="A python package to retrieve your bankin account balances, store them periodically and display "
-                "them in a beautifull graph",
+                "them in a beautiful graph",
     url="https://github.com/Gamma-Software/GetUpdates",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
+    entry_points={"console_scripts": ["show_balance=show_balance:main",
+                                      "store_balance=store_balance:main"]},
     python_requires='>=3.7',
 )
