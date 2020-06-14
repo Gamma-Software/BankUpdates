4af51ea788bcd9425dce582d78cd950ad85a05b3
Valentin Rudloff <valentin.rudloff.perso@gmail.com>
2020-06-14 17:44:00 +0200

    get the correct module requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,18 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+ignored_dependencies = []
+
+
+def get_dependencies():
+    with open("requirements.txt", "r") as fh:
+        requirements = fh.read()
+        requirements = requirements.split('\n')
+        map(lambda r: r.strip(), requirements)
+        requirements = [r for r in requirements if r not in ignored_dependencies]
+        return requirements
+
+
 setuptools.setup(
     name="get-account",
     version="1.0.0",
@@ -16,6 +28,9 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requirements=get_dependencies(),
     entry_points={"console_scripts": ["show_balance=show_balance:main",
                                       "store_balance=store_balance:main"]},
     python_requires='>=2.7',
