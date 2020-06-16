2af8a37ec0e4612d6785b6bc1143e99676fd3c59
Valentin Rudloff <valentin.rudloff.perso@gmail.com>
2020-06-16 23:18:34 +0200

    rename main and adapt to parse/authenticate on onedrive

--- store_balance.py
+++ store_balance.py
@@ -1,22 +1,31 @@
 from get_account_api.bankin_interface import BankinInterface
 from get_account_api.exceptions import PostGetErrors
 from get_account_api.excel_interface import ExcelInterface
+from get_account_api.onedrive_interface import OnedriveInterface
+import get_account_api.parameters_parsing as conf
 import getpass
+import os
 
 
 def store_balance():
     """ Main script to login, refresh the balance, save it in an excel file, logout"""
 
-    # Open the login file and retrieve the personal data to login to Bankin account
-    f = open("login.txt", "r")
-    [email, client_id, client_secret] = f.read().splitlines()
-    f.close()
+    # Open the login file and retrieve the personal data to login to Bankin account and Onedrive
+    bankin_param = conf.parse_bankin_params("login/bankin_oauth.yml")
+    onedrive_param = conf.parse_onedrive_params("login/onedrive_oauth.yml")
 
     # Get the password in the console
     password = getpass.getpass('Type your bankin password: ')
-
+    
     # Use the Bankin interface to login, refresh the balance, save it in an excel file, logout
-    bankin_interface = BankinInterface(email, password, client_id, client_secret)
+    bankin_interface = BankinInterface(bankin_param["email"], password,
+                                       bankin_param["client_id"], bankin_param["client_secret"])
+    onedrive_interface = OnedriveInterface(onedrive_param["client_id"], onedrive_param["client_secret"],
+                                           onedrive_param["onedrive_uri"])
+
+    # Authenticate to onedrive
+    onedrive_interface.authenticate()
+
     try:
         if bankin_interface.authenticate():
             if bankin_interface.refresh_items(bankin_interface.get_items_ids()):
@@ -24,14 +33,10 @@ def store_balance():
                 bankin_interface.logout()
                 excel_interface = ExcelInterface('accounts.xlsx')
                 excel_interface.save_in_excel(data)
+                onedrive_interface.upload_file("accounts.xlsx", os.path.join(os.path.dirname(__file__), 'accounts.xlsx'))
     except PostGetErrors as error:
         print("error: " + error.message)
 
 
-def main():
-    # execute only if run as a script
-    store_balance()
-
-
 if __name__ == "__main__":
-    main()
+    store_balance()
