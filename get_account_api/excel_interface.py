aa532fb2dbbbb4a161248174938a56c2c26d34b0
Valentin Rudloff <valentin.rudloff.perso@gmail.com>
2020-06-19 00:23:34 +0200

    use path_file

--- get_account_api/excel_interface.py
+++ get_account_api/excel_interface.py
@@ -3,8 +3,9 @@ from os import path
 
 
 class ExcelInterface:
-    def __init__(self, file_name):
+    def __init__(self, path, file_name):
         self.file_name = file_name
+        self.path = path
 
     def clean_data(self, data):
         # Get the first item to create the base of the data frame
@@ -37,12 +38,12 @@ class ExcelInterface:
     def read_excel_in_pd(self):
         """ Read the current dataframe"""
         current_dataframe = pd.DataFrame()
-        if path.exists(self.file_name):
-            current_dataframe = pd.read_excel(self.file_name, index_col=None)
+        if path.exists(self.path):
+            current_dataframe = pd.read_excel(self.path, index_col=None)
         return current_dataframe
 
     def save_in_excel(self, data):
-        writer = pd.ExcelWriter(self.file_name, engine='xlsxwriter',
+        writer = pd.ExcelWriter(self.path, engine='xlsxwriter',
                                 datetime_format='mmm d yyyy hh:mm:ss',
                                 date_format='mmmm dd yyyy')
 
@@ -51,7 +52,7 @@ class ExcelInterface:
 
         # Read the current dataframe
         if not self.read_excel_in_pd().empty:
-            current_dataframe = pd.read_excel(self.file_name, index_col=None)
+            current_dataframe = pd.read_excel(self.path, index_col=None)
             merged_dataframe = pd.merge(current_dataframe, cleaned_data, on='timestamp', how='outer')
         else:
             merged_dataframe = cleaned_data
