59edd79cde3af15fae09425b671badc1ac0b70c9
Valentin Rudloff <valentin.rudloff.perso@gmail.com>
2020-06-23 09:59:54 +0200

    change " to ' and set clean_data to static

--- get_account_api/excel_interface.py
+++ get_account_api/excel_interface.py
@@ -2,38 +2,39 @@ import pandas as pd
 from os import path
 
 
+def clean_data(data):
+    # Get the first item to create the base of the data frame
+    account = data[0]
+    name = account.get('name')
+    if account.get('custom_name') is not None:
+        name = account.get('custom_name')
+    dataframe_to_return = pd.DataFrame({
+        'timestamp': [pd.Timestamp(account.get('updated_at')).floor('min').tz_convert(None)],
+        name + ': ' + str(account.get('item').get('id')): [account.get('balance')]})
+
+    # Skip the first iteration
+    iteraccount = iter(data)
+    next(iteraccount)
+
+    # Get the other items and merge them in the DataFrame
+    for account in iteraccount:
+        name = account.get('name')
+        if account.get('custom_name') is not None:
+            name = account.get('custom_name')
+        df = pd.DataFrame({
+            'timestamp': [pd.Timestamp(account.get('updated_at')).floor('min').tz_convert(None)],
+            name + ': ' + str(account.get('item').get('id')): [account.get('balance')]})
+        dataframe_to_return = pd.merge(dataframe_to_return, df, on='timestamp', how='outer')
+
+    # Clean dataframe
+    dataframe_to_return.sort_values(by='timestamp', inplace=True)
+    return dataframe_to_return
+
+
 class ExcelInterface:
     def __init__(self, path, file_name):
         self.file_name = file_name
         self.path = path
-
-    def clean_data(self, data):
-        # Get the first item to create the base of the data frame
-        account = data[0]
-        name = account.get('name')
-        if account.get('custom_name') is not None:
-            name = account.get('custom_name')
-        dataframe_to_return = pd.DataFrame({
-            'timestamp': [pd.Timestamp(account.get('updated_at')).floor('min').tz_convert(None)],
-            name + ': ' + str(account.get('item').get('id')): [account.get('balance')]})
-
-        # Skip the first iteration
-        iteraccount = iter(data)
-        next(iteraccount)
-
-        # Get the other items and merge them in the DataFrame
-        for account in iteraccount:
-            name = account.get('name')
-            if account.get('custom_name') is not None:
-                name = account.get('custom_name')
-            df = pd.DataFrame({
-                'timestamp': [pd.Timestamp(account.get('updated_at')).floor('min').tz_convert(None)],
-                name + ': ' + str(account.get('item').get('id')): [account.get('balance')]})
-            dataframe_to_return = pd.merge(dataframe_to_return, df, on='timestamp', how='outer')
-
-        # Clean dataframe
-        dataframe_to_return.sort_values(by='timestamp', inplace=True)
-        return dataframe_to_return
 
     def read_excel_in_pd(self):
         """ Read the current dataframe"""
@@ -48,7 +49,7 @@ class ExcelInterface:
                                 date_format='mmmm dd yyyy')
 
         # Clean the data frame
-        cleaned_data = self.clean_data(data)
+        cleaned_data = clean_data(data)
 
         # Read the current dataframe
         if not self.read_excel_in_pd().empty:
