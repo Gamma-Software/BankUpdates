d43f5a7e456ff60194ad3c35622e9d92b83c82c1
Valentin Rudloff <valentin.rudloff.perso@gmail.com>
2020-07-21 22:24:28 +0200

    update v2.0.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_dependencies():
 
 setuptools.setup(
     name='get-account',
-    version='2.0.1',
+    version='2.0.2',
     author='Valentin Rudloff',
     author_email='valentin.rudloff.perso@gmail.com',
     description='A python package to retrieve your bankin account balances, store them periodically and display '
